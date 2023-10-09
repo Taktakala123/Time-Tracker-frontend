1d6da0026b585798994a434d744fa981cdd93904
ala TAKTAK <ala.taktak@tekab.dev>
2023-10-09 16:34:33 +0000

    wip

--- client-ui/src/App.vue
+++ client-ui/src/App.vue
@@ -1,27 +1,66 @@
 <template>
+   <div class="flex justify-content-start flex-wrap">
+      <Date />
+   </div>
 
-<div class="flex justify-content-start flex-wrap">
-    <Date/>
-</div>
+   <div class="flex justify-content-end flex-wrap">
+      <div class="flex align-items-center justify-content-center m-2">
+         <StartButton />
+      </div>
+      <div class="flex align-items-center justify-content-center m-2">
+         <StopButton />
+      </div>
+   </div>
 
-<div class="flex justify-content-end flex-wrap">
- <div class="flex align-items-center justify-content-center m-2">
-    <StartButton :myEvent="()=>console.log('test')"/>
- </div>
- <div class="flex align-items-center justify-content-center m-2">
-    <StopButton />
- </div>
-</div>
+   <div class="card">
+      <div v-for="time in times" :key="time.id">
+         Period {{ time.id }}
+         {{ formatTime(time.StartTime) }} - {{ formatTime(time.EndTime) }}
+      </div>
+   </div>
 
-<TimeLog/>
-    
 </template>
 
-<script setup lang="ts">
+
+<script lang="ts">
 import Date from './components/Date.vue';
 import StartButton from './components/StartButton.vue';
 import StopButton from './components/StopButton.vue';
-import TimeLog from './components/TimeLog.vue';
+import { defineComponent } from "vue";
+import service from "../service/index"
+export default defineComponent({
+   name: "TimeLog",
+   components: {
+      Date,
+      StartButton,
+      StopButton,
+   },
+    data() {
+      return {
+         times: {},
+         time: {
+            id: null,
+            StartTime: null,
+
+         },
+      };
+   },
+   mounted: async function () {
+      try {
+         const response = await service.timeLogControllerFindAll({ format: 'json' });
+         console.log(response);
+         this.times = response.data;
+      }
+      catch (error) {
+         console.log(error);
+      }
+   },
+   methods: {
+      formatTime(timeStr: Date) {
+         const options = { hour: "numeric", minute: "numeric" };
+         return new Date(timeStr).toLocaleTimeString("en-US", options);
+      },
+   },
+
+});
 </script>
-
-
--- client-ui/src/components/TimeLog.vue
+++ client-ui/src/components/TimeLog.vue
@@ -1,42 +1,15 @@
 <template>
-<div class="card">
-    <div v-for="time in times" :key="time.id">
-      Period {{ time.id }}
-      {{ formatTime(time.StartTime) }} - {{ formatTime(time.EndTime) }}
-      {{ time.duration }}
-    </div>
-  </div>
+
   
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import service from "../../service/index"
-import StopButton from "./StopButton.vue";
 export default defineComponent({
     name: "TimeLog",
     data() {
-        return {
-            times: null,
-            timeID: Number,
-        };
+        
     },
-    mounted: async function () {
-        try {
-            const response = await service.timeLogControllerFindAll({ format: 'json' });
-            console.log(response);
-            this.times = response.data;
-        }
-        catch (error) {
-            console.log(error);
-        }
-    },
-    methods: {
-        formatTime(timeStr: Date) {
-            const options = { hour: "numeric", minute: "numeric" };
-            return new Date(timeStr).toLocaleTimeString("en-US", options);
-        },
-    },
-    components: { StopButton }
+    
 });
 </script>
