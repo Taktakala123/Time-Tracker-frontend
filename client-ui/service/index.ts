73609d2523c078db9196e13ae36d09bb7851454a
ala TAKTAK <ala.taktak@tekab.dev>
2023-10-09 15:21:35 +0000

    wip

--- client-ui/service/index.ts
+++ client-ui/service/index.ts
@@ -1,7 +1,7 @@
 import { Api } from "../index"
 
 const service = new Api ({
-    baseUrl: 'https://3000-taktakala12-timetracker-3q7zv7f94v2.ws-eu105.gitpod.io'
+    baseUrl: 'https://3000-taktakala12-timetracker-cpkgv53024h.ws-eu105.gitpod.io'
 })
 
 export default service
